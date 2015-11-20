c2b461af78f69ab2a78e2a2762ce25faf9cdeb84
Tetiana Hogg <tetiana.hogg@alliants.com>
2015-11-20 23:59:19 +0000

    Display nicely name typed and add key mapping

--- web/static/js/states/menu.js
+++ web/static/js/states/menu.js
@@ -1,4 +1,5 @@
 import {Socket} from "phoenix"
+import {Keyboard} from "./key_map"
 
 //Phoenix sockets and channels
 class App {
@@ -24,13 +25,20 @@ class App {
 //Menu with Phaser
 export class MenuState extends Phaser.State {
   create() {
-    this.label = this.addText('Type your name', this.world.centerX, this.world.centerY - 100)
+    this.label = this.addText('Type your name\n and press enter', this.world.centerX, this.world.centerY - 100)
     this.input.keyboard.addCallbacks(this, this.onDown)
+    this.fullName = ""
+    this.keyboard = new Keyboard ()
+    this.xValue = -80
   }
 
-  addText(message, x, y, style = { font: '74px Arial Black', fill: '#ff69b4' }) {
+  getName(name) {
+    console.log("name", name)
+  }
+
+  addText(message, x, y, style = { font: '54px Arial Black', fill: '#ff69b4' }) {
     let text = this.add.text(x, y, message, style)
-    text.stroke = "#de77ae"
+    text.stroke = "#260d19"
     text.strokeThickness = 16
     text.setShadow(2, 2, "#333333", 2, true, true)
     text.anchor.setTo(0.5)
@@ -38,22 +46,23 @@ export class MenuState extends Phaser.State {
   }
 
   onDown(e) {
-    this.recordLetter(e.keyCode)
+    if (e.which == 13) {
+      console.log("here we should send message to server with name, return from this method and start the game")
+    }
+
+    console.log(e.which)
+    let character = this.keyboard.charFromCode(e.which, e.shiftKey)
+    this.recordLetter(character)
   }
 
   recordLetter(keyCode) {
+    //send message with name to server if someone hits enter
     console.log(keyCode)
-    let name = this.addText(String.fromCharCode(keyCode), this.world.centerX, this.world.centerY)
-    let tween = this.add.tween(name)
-    tween.to({ alpha: 0, x: 500, y: 30 }, 2000, Phaser.Easing.Linear.None)
-    tween.onComplete.add(function () {
-      name.destroy()
-    })
-    tween.start()
+    let name = this.addText(keyCode, this.world.centerX + this.xValue, this.world.centerY + 50)
+    this.xValue = this.xValue + 50
 
-    let fullName = ""
-    fullName = fullName + name._text
-    console.log(fullName)
+    this.fullName = this.fullName + name._text
+    console.log(this.fullName)
   }
 
   update() {
