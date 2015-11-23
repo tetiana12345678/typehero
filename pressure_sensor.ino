0e06eb905439d4101ca5003f16c1cea3732ff9d8
Tetiana Hogg <tetiana.hogg@alliants.com>
2015-11-23 23:34:19 +0000

    C program for 2 sensative fingers

--- pressure_sensor.ino
+++ pressure_sensor.ino
@@ -1,10 +1,12 @@
 /******************************************/
-const int analogPin = A0;//the analog input pin attach to
- 
-int inputValue = 0;//variable to store the value coming from sensor
-int outputValue = 0;//variable to store the output value
+const int analogPin = A0;//index finger
+const int analogPin1 = A1; // middle finger
+
+int inputValue = 0;//value coming from sensor A0, index finger
+int inputValue1 = 0; //value from sensor A1, middle finger
 
 bool wasDown = false;
+bool wasDown1 = false;
 /******************************************/
 void setup()
 {
@@ -14,19 +16,25 @@ void setup()
 void loop()
 {
   inputValue = analogRead(analogPin);//read the value from the sensor
-  // print it out in many formats:
-  if (inputValue > 500 && !wasDown) {
+
+  if (inputValue > 900 && !wasDown) {
     wasDown = true;
-    Serial.println(analogPin);
-  } else if (inputValue <= 500) {
+    Serial.println("Index finger");
+  } else if (inputValue <= 900) {
     wasDown = false;
   }
-  
-//  Serial.println(inputValue);       // print as an ASCII-encoded decimal
 
-  // delay 10 milliseconds before the next reading:
-  delay(10);
- // outputValue = map(inputValue,0,1023,0,255);//Convert from 0-1023 proportional to the number of a number of from 0 to 255
-  
+   inputValue1 = analogRead(analogPin1);
+
+   if (inputValue1 > 100 && !wasDown1) {
+    wasDown1 = true;
+    Serial.println("Fuck finger");
+  } else if (inputValue1 <= 100) {
+    wasDown1 = false;
+  }
+
+  //Serial.println(inputValue1);       // print as an ASCII-encoded decimal
+
+
 }
 /*******************************************/
