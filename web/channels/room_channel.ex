a9e3a6ccc1008a215216954adf926685dd4e62c6
Tetiana Hogg <tetiana.hogg@alliants.com>
2016-06-14 23:03:33 +0100

    remove not used for the game channels

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -2,25 +2,8 @@ defmodule Typehero.RoomChannel do
   use Phoenix.Channel
 
   def join("rooms:lobby", msg, socket) do
-    {:ok, serial} = Serial.start_link
-
-    Serial.open(serial, "/dev/cu.usbmodem1411")
-    Serial.set_speed(serial, 9600)
-    Serial.connect(serial)
     # socket = assign(socket, :key_index, 0)
     {:ok, socket}
-  end
-
-  def handle_info({:elixir_serial, serial, data}, state) do
-    #do something with data.
-    cond do
-      data =~ ~r/1/ -> broadcast state, "arduino:finger", %{finger: 1}
-      data =~ ~r/2/ -> broadcast state, "arduino:finger", %{finger: 2}
-      data =~ ~r/3/ -> broadcast state, "arduino:finger", %{finger: 3}
-      true -> nil
-    end
-
-    {:noreply, state}
   end
 
   def handle_in("user:join", msg, socket) do
